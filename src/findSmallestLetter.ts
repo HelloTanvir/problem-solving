2279898fe84d64de3eb2a56bcc69a9e65e4140a7
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-01-18 21:21:30 +0600

    smarter process

--- src/findSmallestLetter.ts
+++ src/findSmallestLetter.ts
@@ -27,3 +27,24 @@ const findSmallestLetter = (letters: string[], target: string): string => {
 };
 
 console.log(findSmallestLetter(letters, target));
+
+// smarter process
+
+const findSmallestLetter2 = (letters: string[], target: string): string => {
+  let start = 0;
+  let end = letters.length - 1;
+
+  while (start <= end) {
+    let mid = Math.floor(start + (end - start) / 2);
+
+    if (target < letters[mid]) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+
+  return letters[start % letters.length];
+};
+
+console.log(findSmallestLetter2(letters, target));
