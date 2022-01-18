bf12256018f5ce0c968d096cf5f71d5c6d6614c0
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-01-18 21:20:57 +0600

    find the smallest letter greater than the target

---
+++ src/findSmallestLetter.ts
@@ -0,0 +1,29 @@
+// find the smallest letter greater than the target
+
+const letters = ["c", "f", "j"];
+const target = "a";
+
+const findSmallestLetter = (letters: string[], target: string): string => {
+  if (target >= letters[letters.length - 1]) {
+    return letters[0];
+  }
+
+  let start = 0;
+  let end = letters.length - 1;
+
+  while (start <= end) {
+    let mid = Math.floor(start + (end - start) / 2);
+
+    if (target > letters[mid]) {
+      start = mid + 1;
+    } else if (target < letters[mid]) {
+      end = mid - 1;
+    } else {
+      return letters[mid + 1];
+    }
+  }
+
+  return letters[start];
+};
+
+console.log(findSmallestLetter(letters, target));
